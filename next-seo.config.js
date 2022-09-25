386dabc9bcadd91a74700661b6f292791d67a3bf
ji23 <t.mac.jun.0303@gmail.com>
2022-09-25 22:55:35 +0900

    テスト用にnoindexを追加

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  noindex: true,
   titleTemplate: '%s | JI23-DEV',
   defaultTitle: 'JI23-DEV',
   additionalMetaTags: [
