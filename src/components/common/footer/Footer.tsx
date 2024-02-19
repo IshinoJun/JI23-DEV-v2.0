cdc0811dff2a60a3cb171ddf5106e9d29be46262
ji23 <t.mac.jun.0303@gmail.com>
2024-02-19 12:45:11 +0900

    fix: footer

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className='py-4 mt-12 text-center text-sm text-gray-300'>
-      &copy; 2022 JI23-DEV
+      &copy; JI23-DEV
     </footer>
   );
 };
